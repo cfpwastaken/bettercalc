b2a31e28f4258df3eb0713b552f93e80fdb52c50
Cfp <chaosflo44plus@gmail.com>
2021-08-24 20:13:07 +0200

    Initial Commit

---
+++ script.js
@@ -0,0 +1,114 @@
+document.addEventListener("DOMContentLoaded", () => {
+    var calculationSmall = document.getElementById("calculationSmall");
+    var calculation = document.getElementById("calculation");
+    var buttons = document.getElementsByClassName("calcButton");
+    for (let i = 0; i < buttons.length; i++) {
+        const button = buttons[i];
+        button.addEventListener("click", () => {
+            if(calculationSmall.innerText.includes("=")) {
+                calculationSmall.innerText = "";
+                calculation.innerText = "0";
+            }
+            if(calculation.innerText == "0" && (button.innerText != "," && button.innerText != "+" && button.innerText != "-" && button.innerText != "*" && button.innerText != "/" && button.innerText != "%" && button.innerText != "=" && button.innerText != "+-")) {
+                calculation.innerText = "";
+            }
+
+
+            if(button.innerText == "C") {
+                calculation.innerText = "0";
+                calculationSmall.innerText = "";
+            } else if(button.innerText == "CE") {
+                calculation.innerText = "0";
+            } else if(button.innerText == "X") {
+                calculation.innerText = calculation.innerText.substring(0, calculation.innerText.length - 1);
+            } else if(button.innerText == "P") {
+                alert("Placeholder");
+            } else if(button.innerText == ",") {
+                if(!calculation.innerText.substring(0, calculation.innerText.length - 1)) {
+                    calculation.innerText += button.innerText;
+                }
+            } else if(button.innerText == "+") {
+                calculationSmall.innerText += " " + calculation.innerText + " + ";
+                calculation.innerText = "0";
+            } else if(button.innerText == "-") {
+                calculationSmall.innerText += " " + calculation.innerText + " - ";
+                calculation.innerText = "0";
+            } else if(button.innerText == "*") {
+                calculationSmall.innerText += " " + calculation.innerText + " * ";
+                calculation.innerText = "0";
+            } else if(button.innerText == "/") {
+                calculationSmall.innerText += " " + calculation.innerText + " / ";
+                calculation.innerText = "0";
+            } else if(button.innerText == "%") {
+                calculationSmall.innerText += " " + calculation.innerText + " % ";
+                calculation.innerText = "0";
+            } else if(button.innerText == "+-") {
+                if(calculation.innerText.startsWith("-")) {
+                    calculation.innerText = calculation.innerText.substring(1, calculation.innerText.length);
+                } else {
+                    calculation.innerText = "-" + calculation.innerText;
+                }
+            } else if(button.innerText == "=") {
+                calculationSmall.innerText += " " + calculation.innerText;
+                if(calculationSmall.innerText.startsWith("0") && !calculationSmall.innerText.startsWith("0,") && !calculationSmall.innerText.startsWith("0 +") && !calculationSmall.innerText.startsWith("0 -")  && !calculationSmall.innerText.startsWith("0 *")  && !calculationSmall.innerText.startsWith("0 /")  && !calculationSmall.innerText.startsWith("0 %")) {
+                    calculationSmall.innerText = calculationSmall.innerText.substring(1, calculationSmall.innerText.length);
+                }
+                calculationSmall.innerText = calculationSmall.innerText.replaceAll(",", ".");
+                // calculation.innerText = eval(calculationSmall.innerText);
+                calculation.innerText = calc(calculationSmall.innerText);
+                calculationSmall.innerText = calculationSmall.innerText.replaceAll(".", ",") + " = ";
+            } else {
+                calculation.innerText += button.innerText;
+            }
+            if(calculation.innerText == "") {
+                calculation.innerText = "0";
+            }
+        });
+    }
+});
+
+
+function calc(str) {
+    // str = str.replace(/\s/g, '');
+
+    if(str.includes("(")) return "Unsupported."; // For funcs and (more math here (prob gonna use recursive for this but i dont have the buttons for that yet soooo))
+    // if(str.includes("-")) return "Help";
+
+    var stuff = str.split(/\s/g);
+    var temp = [];
+    var result = 0;
+
+    for (let i = 0; i < stuff.length; i++) {
+        const elmnt = stuff[i];
+        if(elmnt == "*" || elmnt == "/" || elmnt == "%") {
+            temp.push(stuff[i - 1]);
+            temp.push(stuff[i]);
+            temp.push(stuff[i + 1]);
+        }
+    }
+    for (let i = 0; i < stuff.length; i++) {
+        const elmnt = stuff[i];
+        if(elmnt == "+" || elmnt == "-") {
+            temp.push(stuff[i - 1]);
+            temp.push(stuff[i]);
+            temp.push(stuff[i + 1]);
+        }
+    }
+    stuff = temp;
+
+    for (let i = 0; i < stuff.length; i++) {
+        if(stuff[i] == "+") {
+            result += Number(stuff[i - 1]) + Number(stuff[i + 1]);
+        } else if(stuff[i] == "-") {
+            result += Number(stuff[i - 1]) - Number(stuff[i + 1]);
+        } else if(stuff[i] == "*") {
+            result += Number(stuff[i - 1]) * Number(stuff[i + 1]);
+        } else if(stuff[i] == "/") {
+            result += Number(stuff[i - 1]) / Number(stuff[i + 1]);
+        } else if(stuff[i] == "%") {
+            result += Number(stuff[i - 1]) % Number(stuff[i + 1]);
+        }
+    }
+
+    return result;
+}
